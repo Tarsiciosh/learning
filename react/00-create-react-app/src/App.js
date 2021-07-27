17593efc786d6f18a1f5baf0cd9062980673d77b
Tar <tarsiciosh@gmail.com>
2021-07-27 17:26:53 -0300

    learning more react

---
+++ react/00-create-react-app/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
+          Learn React
+        </a>
+
+        <ul> 
+            <li>item 1 </li>
+            <li>item 2 </li> 
+        </ul>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/README.md
@@ -0,0 +1,12 @@
+# Recommended React Toolchains  
+## Create React App
+- > npx create-react-app 00-create-react-app
+- > cd 00-create-react-app
+- > npm start or yarn start
+
+
+## Next.js 
+- > npx create-next-app 01-nextjs-blog --use-npm --example "https://github.com/vercel/next-learn-starter/tree/master/learn-starter" 
+- > cd 01-nextjs-blog
+- > npm run dev
+
